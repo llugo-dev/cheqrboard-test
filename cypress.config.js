ab16080e24b713aaee60904a59fdfbd3187db7e7
Luis Lugo <dragonlugo@gmail.com>
2023-10-27 15:19:10 -0600

    Testing on Chrome

--- cypress.config.js
+++ cypress.config.js
@@ -8,9 +8,6 @@ module.exports = defineConfig({
     specPattern: [
       'cypress/integration/tests/*.js'
     ],
-    projectId: "9j7yvc",
-    env: {
-      CYPRESS_RECORD_KEY: "d2b61efd-f743-460b-8dd5-2e09128efc1b"
-    }
+    projectId: "9j7yvc"
   },
 });
